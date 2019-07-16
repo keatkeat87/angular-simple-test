6d4cc3a9950b6a27735012c1c402338ce9bf52dc
兴杰 严 <keatkeat87@live.com>
2019-07-16 18:26:42 +0800

    ok

--- src/app/test-stepper/test-stepper.component.ts
+++ src/app/test-stepper/test-stepper.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-test-stepper',
   templateUrl: './test-stepper.component.html',
-  styleUrls: ['./test-stepper.component.css']
+  styleUrls: ['./test-stepper.component.scss']
 })
 export class TestStepperComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  formGroup: FormGroup;
 
   ngOnInit() {
+
+    this.formGroup = this.formBuilder.group({
+      info: this.formBuilder.group({
+        name: ['', Validators.required]
+      }),
+      address: this.formBuilder.group({
+        address1: ['', Validators.required]
+      })
+    });
   }
 
 }
--- src/app/test-stepper/test-stepper.module.ts
+++ src/app/test-stepper/test-stepper.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { TestStepperRoutingModule } from './test-stepper-routing.module';
 import { TestStepperComponent } from './test-stepper.component';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
 
 
 @NgModule({
   declarations: [TestStepperComponent],
   imports: [
     CommonModule,
-    TestStepperRoutingModule
+    ReactiveFormsModule,
+    TestStepperRoutingModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class TestStepperModule { }
